2539055c6a970361656028ad6e1888e3ef5d1ff8
cullyfung <cullyfung@gamil.com>
2023-09-03 23:51:29 +0800

    feat: config plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.spelllang = "en,cjk"
+opt.scrolloff = 5
+opt.spelloptions = "camel"
+opt.indentexpr = ""
+opt.foldmethod = "indent"
+opt.foldlevel = 99
+opt.foldenable = true
+opt.foldlevelstart = 99
+opt.relativenumber = false
